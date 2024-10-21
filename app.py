81b8b66a88e39c75544c2be8cd5f53940eb5deb5
Nestorfron <nestorfrones07@hotmail.com>
2024-10-21 18:52:39 -0300

    gunicorn2

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, redirect, url_for
+from flask import Flask, redirect, url_for, send_from_directory
 from extensions import db
 from flask_cors import CORS
 from backend.routes import api_blueprint
@@ -8,13 +8,13 @@ from flask_admin.contrib.sqla import ModelView
 from backend.models import Item  # Asegúrate de tener el modelo definido en models.py
 from dotenv import load_dotenv
 import os
- 
 
 # Cargar variables de entorno desde el archivo .env
 load_dotenv()
 
 def create_app():
-    app = Flask(__name__)
+    app = Flask(__name__, static_folder='dist', static_url_path='')  # Cambia a 'dist'
+
     app.config.from_object(Config)
 
     # Inicializar la base de datos
@@ -30,14 +30,20 @@ def create_app():
     admin = Admin(app, name='Admin Panel', template_mode='bootstrap3')
     admin.add_view(ModelView(Item, db.session))  # Agrega el modelo Item al panel de administración
 
-    # Redirigir la ruta raíz al panel de administración
+    # Ruta para el frontend
     @app.route('/')
-    def index():
-        return redirect(url_for('admin.index'))  # Redirige a la vista principal de Flask-Admin
+    def serve_frontend():
+        return send_from_directory(app.static_folder, 'index.html')
+
+    # Ruta para servir otros archivos estáticos (CSS, JS, etc.)
+    @app.route('/<path:path>')
+    def send_static(path):
+        return send_from_directory(app.static_folder, path)
 
     return app
 
 if __name__ == '__main__':
     app = create_app()
     PORT = int(os.environ.get('PORT', 3001))
-    app.run(host='0.0.0.0', port=PORT, debug=True)
+    debug_mode = os.environ.get('FLASK_DEBUG', '0') == '1'
+    app.run(host='0.0.0.0', port=PORT, debug=debug_mode)  # Usar la variable de entorno para debug
