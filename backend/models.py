4cd2c8e2584ad13984d34ff31dcad0f288483977
Nestorfron <nestorfrones07@hotmail.com>
2024-10-21 09:01:08 -0300

    second

---
+++ backend/models.py
@@ -0,0 +1,10 @@
+from extensions import db
+
+class Item(db.Model):
+    __tablename__ = 'items'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f'<Item {self.name}>'
---
+++ backend/routes.py
@@ -0,0 +1,19 @@
+from flask import Blueprint, jsonify, request
+from models import Item
+from extensions import db
+
+api_blueprint = Blueprint('api', __name__)
+
+@api_blueprint.route('/items', methods=['GET'])
+def get_items():
+    items = Item.query.all()
+    items_list = [{"id": item.id, "name": item.name} for item in items]
+    return jsonify(items_list)
+
+@api_blueprint.route('/items', methods=['POST'])
+def add_item():
+    data = request.json
+    new_item = Item(name=data['name'])
+    db.session.add(new_item)
+    db.session.commit()
+    return jsonify({"message": "Item added successfully!"}), 201
