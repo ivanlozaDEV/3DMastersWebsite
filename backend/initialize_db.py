9318e649bd9d79ab9bc5b6e5a713afecbc6095ab
Nestorfron <nestorfrones07@hotmail.com>
2024-10-21 20:57:23 -0300

    database12

--- backend/initialize_db.py
+++ backend/initialize_db.py
@@ -5,7 +5,7 @@ from extensions import db  # Asegúrate de que esto apunte a la extensión corre
 # Agregar el directorio backend al path de Python
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'backend')))
 
-from backend import create_app  # Importar la función create_app
+from app import create_app  # Importar la función create_app
 from flask_migrate import upgrade, migrate  # Asegúrate de importar migrate si lo necesitas
 
 def initialize_database():
