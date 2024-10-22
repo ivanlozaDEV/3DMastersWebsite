ae3ab82013828b7548a303908927a73f6fab9920
Nestorfron <nestorfrones07@hotmail.com>
2024-10-21 23:13:22 -0300

    tailwind y nextui

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
-import React from 'react';
+import React from "react";
+import { NextUIProvider } from "@nextui-org/react";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import injectContext from '../src/store/appContext.jsx';
+import injectContext from "../src/store/appContext.jsx";
 
+import Home from "./pages/home.jsx";
+import Navbar from "./components/navbar.jsx";
 
-
-import Home from './pages/home.jsx';
-
-
-
-
-
-
-function App() {
+function App({ Component, pageProps }) {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route element={<h1>Not found!</h1>} />
-      </Routes>
-    </BrowserRouter>
+    <NextUIProvider>
+      <NextThemesProvider attribute="class" defaultTheme="dark">
+        <BrowserRouter {...pageProps}>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route element={<h1>Not found!</h1>} />
+          </Routes>
+        </BrowserRouter>
+      </NextThemesProvider>
+    </NextUIProvider>
   );
 }
 
